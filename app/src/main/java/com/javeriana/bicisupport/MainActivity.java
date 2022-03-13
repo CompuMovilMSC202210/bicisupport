beae927633038cc904adc72e433dc10e18958040
Miguel Gutiérrez <migueangel97@hotmail.com>
2022-03-13 16:11:12 -0500

    Finished login activity

--- app/src/main/java/com/javeriana/bicisupport/MainActivity.java
+++ app/src/main/java/com/javeriana/bicisupport/MainActivity.java
@@ -2,11 +2,15 @@ package com.javeriana.bicisupport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        loginButton = findViewById(R.id.loginButton);
     }
 }
