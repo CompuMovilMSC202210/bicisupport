51fde86f79ed613921730449757e4f2288ac24de
John Garcia <john_garcia@javeriana.edu.co>
2022-03-15 10:41:37 -0500

    Creacion de la pantalla de cambiar datos dela bicilceta

--- app/src/main/java/com/javeriana/bicisupport/MainActivity.java
+++ app/src/main/java/com/javeriana/bicisupport/MainActivity.java
@@ -8,18 +8,26 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button go;
+    Button go, gob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         go = findViewById(R.id.button);
-
+        gob = findViewById(R.id.button4);
         go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), EncuestaSatisfaccionChat.class);
+                startActivity(intent);
+            }
+        });
+
+        gob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), DetalleBicicleta.class);
                 startActivity(intent);
             }
         });
