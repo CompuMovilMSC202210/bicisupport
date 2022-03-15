1d2e65189cddfc73cb93a3d7f3edfc12dd8f7c90
John Garcia <john_garcia@javeriana.edu.co>
2022-03-14 21:58:59 -0500

    Cracion de la pantalla de la encuesta de satisfaccion

--- app/src/main/java/com/javeriana/bicisupport/EncuestaSatisfaccionChat.java
+++ app/src/main/java/com/javeriana/bicisupport/EncuestaSatisfaccionChat.java
@@ -7,21 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class EncuestaSatisfaccionChat extends AppCompatActivity {
     Button go;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        go = findViewById(R.id.button);
+        setContentView(R.layout.activity_encuesta_satisfaccion_chat);
+        go =findViewById(R.id.enviarEncuesta);
 
         go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), EncuestaSatisfaccionChat.class);
-                startActivity(intent);
+                //Intent intent = new Intent(view.getContext(), ListaIncidentes.class);
+                //startActivity(intent);
             }
         });
     }
+
+
 }
