832ae6393aa3150691e4c4c049f2d0506f0472c6
John Garcia <john_garcia@javeriana.edu.co>
2022-03-15 10:43:11 -0500

    Eliminacion de datos innecesarios pantalla principal

--- app/src/main/java/com/javeriana/bicisupport/MainActivity.java
+++ app/src/main/java/com/javeriana/bicisupport/MainActivity.java
@@ -2,34 +2,15 @@ package com.javeriana.bicisupport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button go, gob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        go = findViewById(R.id.button);
-        gob = findViewById(R.id.button4);
-        go.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), EncuestaSatisfaccionChat.class);
-                startActivity(intent);
-            }
-        });
 
-        gob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), DetalleBicicleta.class);
-                startActivity(intent);
-            }
-        });
     }
 }
