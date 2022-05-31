5292c6a5d1e35c91f19ac116e3a8c5847ef35d45
Miguel Gutiérrez <migueangel97@hotmail.com>
2022-05-30 19:51:07 -0500

    Adding message service

--- app/src/main/java/com/javeriana/bicisupport/activities/HomeActivity.java
+++ app/src/main/java/com/javeriana/bicisupport/activities/HomeActivity.java
@@ -2,37 +2,51 @@ package com.javeriana.bicisupport.activities;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.gson.Gson;
 import com.javeriana.bicisupport.R;
 import com.javeriana.bicisupport.fragments.ChatFragment;
-import com.javeriana.bicisupport.fragments.MapFragment;
 import com.javeriana.bicisupport.fragments.ListaAliadosFragment;
+import com.javeriana.bicisupport.fragments.MapFragment;
 import com.javeriana.bicisupport.fragments.ProfileFragment;
 import com.javeriana.bicisupport.fragments.TipsFragment;
+import com.javeriana.bicisupport.models.requests.UserRequest;
 
 import org.osmdroid.api.IMapController;
-import org.osmdroid.config.Configuration;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
 
-    ImageButton assistant, help, profile, allies,home;
+    ImageButton assistant, help, profile, allies, home;
     private MapView map;
     private IMapController mapController;
+
+    SharedPreferences prefs;
+    RequestQueue requestQueue;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -40,7 +54,18 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        prefs = getSharedPreferences(this.getString(R.string.app_name), Context.MODE_PRIVATE);
+        String localId = prefs.getString("localId", "");
+
         Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(HomeActivity.this, new OnSuccessListener<InstanceIdResult>() {
+            @Override
+            public void onSuccess(InstanceIdResult instanceIdResult) {
+                String updatedToken = instanceIdResult.getToken();
+                updateUser(localId, updatedToken);
+            }
+        });
 
         assistant = findViewById(R.id.assistantButton);
         help = findViewById(R.id.helpButton);
@@ -100,5 +125,35 @@ public class HomeActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 
         fragmentTransaction.replace(R.id.fragmentContainerView, mapFragment).commit();
+    }
+
+    private void updateUser(String localId, String chatToken) {
+        String baseUpdateUserUrl =
+                String.format("https://bici-support-api.herokuapp.com/api/v1/users/%s", localId);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        StringRequest request = new StringRequest(Request.Method.PUT, baseUpdateUserUrl,
+                response -> {
+                    UserRequest userRequest = new Gson().fromJson(response, UserRequest.class);
+                    Log.i("Updated user", userRequest.toString());
+                },
+                error -> {
+                    AlertDialog alert = new AlertDialog.Builder(getApplicationContext())
+                            .setMessage("Ha ocurrido un error actualizando los datos")
+                            .setCancelable(false)
+                            .setPositiveButton("Ok", (dialog, which) -> dialog.cancel())
+                            .create();
+                    alert.setTitle("Error en el registro");
+                    alert.show();
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("cloudToken", chatToken);
+                return params;
+            }
+        };
+
+        requestQueue.add(request);
     }
 }
